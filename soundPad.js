31dcae91cc63c3bbeb09533c05185f21f1c36465
Lucas Marques <41039907+Lucas-Prog@users.noreply.github.com>
2021-04-25 21:32:46 -0300

    Update soundPad.js

--- soundPad.js
+++ soundPad.js
@@ -3,7 +3,7 @@ const createSound = require('./routes/createSound');
 const client =  new Discord.Client();
 const fs = require('fs');
 
-const Token =  ENV.PROCESS.TOKEN;
+const Token =  process.env.TOKEN;
 const prefix = '.';
 client.on('ready', ()=>{
     console.log(`Logged in as ${client.user.tag}!`);
